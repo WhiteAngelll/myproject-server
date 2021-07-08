8db0cbbc31fe1c66d403405dfdd6cdc940775d2a
WhiteAngelll <64792391+WhiteAngelll@users.noreply.github.com>
2021-07-09 00:30:29 +0300

    lesson2

--- products/views.py
+++ products/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+from products.models import Product, ProductCategory
+
+
 def index(request):
-    return render(request, 'products/index.html')
+    context = {'title': 'GeekShop'}
+    return render(request, 'products/index.html', context)
+
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'GeekShop - Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
